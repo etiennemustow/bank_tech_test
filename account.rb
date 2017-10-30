9d00b9fcd1cb96180a09e7891487c25357a37a01
etiennemustow <etienne.o.mustow@gmail.com>
2017-10-30 15:37:16 +0000

    Spiked deposit, withdraw and statement method

--- account.rb
+++ account.rb
@@ -6,9 +6,22 @@ class Account
   def initialize
     @balance = 0
     @date = DateTime.now.strftime "%d/%m/%Y"
+    @transaction = ["date  ||  credit  ||  debit  ||  balance", "#{@date} ||  #{@credit}  ||  #{@debit}  ||  #{@balance}"]
   end
 
   def statement
-    return "date  ||  credit  ||  debit  ||  balance""\n""#{@date} ||  #{@credit}  ||  #{@debit}  ||  #{@balance}"
+    puts @transaction.join("\n")
+  end
+
+  def deposit(amount)
+    @balance += amount
+    @debit = amount
+    @transaction.push("#{@date} ||  #{@credit}  ||  #{@debit}  ||  #{@balance}")
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+    @credit = amount
+    @transaction.push("#{@date} ||  #{@credit}  ||  #{@debit}  ||  #{@balance}")
   end
 end
